d9422fd662755b2098cfe863b381f76636ceb359
Raymond Pendergraph <rpendergraph@cirdacence.com>
2024-03-27 17:59:56 -0400

    Start refactor

--- semantic_scripture/utilities/__init__.py
+++
@@ -1,5 +0,0 @@
-__author__ = 'Ray Pendergraph'
-
-from .read_kjv_text import read_kjv_text
-from .read_bible_names import read_bible_names
-
--- semantic_scripture/utilities/read_bible_names.py
+++
@@ -1,6 +0,0 @@
-
-BIBLE_NAMES = 'data/bible_names.txt'
-
-def read_bible_names():
-    with open(BIBLE_NAMES) as file:
-        return {tuple[0]:tuple[1] for tuple in (line.strip().split('|') for line in file if line)}
